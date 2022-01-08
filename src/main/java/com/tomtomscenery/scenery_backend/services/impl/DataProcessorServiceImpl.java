b5ceb1321244a20db931a8fa22c114f0c13c0df6
RonaldVanDenBurg <60896718+RonaldVanDenBurg@users.noreply.github.com>
2022-01-08 17:53:37 +0100

    Refactored the DataProcessorServiceImpl class

--- src/main/java/com/tomtomscenery/scenery_backend/services/impl/DataProcessorServiceImpl.java
+++ src/main/java/com/tomtomscenery/scenery_backend/services/impl/DataProcessorServiceImpl.java
@@ -8,79 +8,107 @@ import org.json.JSONObject;
 import org.springframework.stereotype.Service;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 
 @Service
-public class DataProcessorServiceImpl implements I_DataProcessorService {
+public class DataProcessorServiceImpl implements I_DataProcessorService
+{
 
-     private final I_UrlBuildService urlBuildService;
+          private final I_UrlBuildService urlBuildService;
 
-     public DataProcessorServiceImpl(I_UrlBuildService urlBuildService) {
-          this.urlBuildService = urlBuildService;
-     }
+          public DataProcessorServiceImpl(I_UrlBuildService urlBuildService) {
+               this.urlBuildService = urlBuildService;
+          }
 
-
-     @Override
-     public PoiImpl readDataReturnPoi() {
-                  try
+          @Override
+          public PoiImpl readDataReturnPoi()
+          {
+               try
                {
-                    URL url = new URL(urlBuildService.getUrlSearchString());
-                    URLConnection hr = url.openConnection();
+                    URL url = getUrl();
+                    URLConnection urlConnection = url.openConnection();
 
-                    if (((HttpURLConnection) hr).getResponseCode() == 200)
+                    if (((HttpURLConnection) urlConnection).getResponseCode() == 200)
                     {
-                         InputStream inputStr = hr.getInputStream();
-                         BufferedReader br = new BufferedReader(new InputStreamReader(inputStr));
-                         String line;
-                         StringBuilder sbuilderObj = new StringBuilder();
-                         while ((line = br.readLine()) != null)
-                         {
-                              sbuilderObj.append(line);
-                         }
+                         InputStream inputStream = urlConnection.getInputStream();
+                         BufferedReader bufferedReaderObject = new BufferedReader(new InputStreamReader(inputStream));
+                         StringBuilder stringBuilderObject = new StringBuilder();
 
-
-                         // Use the JSONObject that contains the json data.
-                              JSONObject jsonObj = new JSONObject(sbuilderObj.toString());
-                         // Create a JsonArrayObject from the "results" array called resultArrayObj.
-                              JSONArray resultArrayObj = jsonObj.getJSONArray("results");
-                         // Create a JSONObject 'poi'. The POI information can be found at index 0 of
-                         // the 'results' array. So use the 'poi' object to search the 'results' array.
-                              JSONObject poi = resultArrayObj.getJSONObject(0).getJSONObject("poi");
-
-                         // Retrieve the String information within the 'poi' JSONObject called 'name'.
-                              String namePoi = poi.getString("name");
-                         // Retriev the Long  information from the 'id' object from the 'categorySet' array
-                         // within the 'poi' object.
-                              JSONArray categorySetArray = poi.getJSONArray("categorySet");
-                              long categoryIdPoi = categorySetArray.getJSONObject(0).getLong("id");
-                         // Then retrieve the String information of the 'freeformAddress' JSONObject
-                         // within the 'poi' JSONObject called 'address'.
-                              JSONObject addressPOI = resultArrayObj.getJSONObject(0).getJSONObject("address");
-                              String addressPoi = addressPOI.getString("freeformAddress");
-
-                         // within the 'poi' JSONObject called 'address' and retrieve the String information
-                         // from the 'localName' object.
-                              JSONObject placeNameObject = resultArrayObj.getJSONObject(0).getJSONObject("address");
-                              String localNamePoi = addressPOI.getString("localName");
-
-
-                         // Retrieve the 'lat' and 'lon' information of type double from the JSONObject called 'position'
-                              JSONObject positionPoi = resultArrayObj.getJSONObject(0).getJSONObject("position");
-                              double latPoi  = positionPoi.getDouble("lat");
-                              double lonPoi  = positionPoi.getDouble("lon");
-
-                         return new PoiImpl(namePoi, categoryIdPoi, addressPoi, localNamePoi, latPoi, lonPoi);
+                         readIncomingData(bufferedReaderObject, stringBuilderObject);
+                         return specificPoiInformation(stringBuilderObject);
                     }
                }
-               catch (Exception e)
-               {
-                    System.out.println(e);
+               catch (MalformedURLException malformedURLException) {
+                    System.out.println(malformedURLException);
                }
-
+               catch (IOException ioException) {
+                    System.out.println(ioException);
+               }
                return null;
           }
-     }
+
+          // This method retrieves specific information from the incoming data,
+          // stores it, and returns it as a PoiImpl object to the frontend.
+          private PoiImpl specificPoiInformation(StringBuilder stringBuilderObject) {
+
+               // Use the JSONObject that contains the json data.
+               JSONObject jsonObj = new JSONObject(stringBuilderObject.toString());
+
+               // Create a JsonArrayObject from the "results" array called resultArrayObj.
+               JSONArray resultArrayObj = jsonObj.getJSONArray("results");
+               //System.out.println("All the result data: " + resultArrayObj.toString());
+
+               // Create a JSONObject 'poi'. The POI information can be found at index 0 of
+               // the 'results' array. So use the 'poi' object to search the 'results' array.
+               JSONObject poi = resultArrayObj.getJSONObject(0).getJSONObject("poi");
+               //System.out.println("All the POI data from the 'poi' object within the 'results' array : " + poi.toString());
+
+               // Retrieve the String information within the 'poi' JSONObject called 'name'.
+               String namePoi = poi.getString("name");
+               //System.out.println("String data from the nameObject within 'poi': " + namePoi);
+
+               // Retriev the Long  information from the 'id' object from the 'categorySet' array
+               // within the 'poi' object.
+               JSONArray categorySetArray = poi.getJSONArray("categorySet");
+               long categoryIdPoi = categorySetArray.getJSONObject(0).getLong("id");
+               //System.out.println("Long data from the categorySetArray within 'poi': " + categoryIdPoi);
+
+
+               // Then retrieve the String information of the 'freeformAddress' JSONObject
+               // within the 'poi' JSONObject called 'address'.
+               JSONObject addressPOI = resultArrayObj.getJSONObject(0).getJSONObject("address");
+               String addressPoi = addressPOI.getString("freeformAddress");
+               //System.out.println("String data from the 'freeformaddress' object within the 'address object within 'poi': " + addressPoi);
+
+               // within the 'poi' JSONObject called 'address' and retrieve the String information
+               // from the 'localName' object.
+               JSONObject placeNameObject = resultArrayObj.getJSONObject(0).getJSONObject("address");
+               String localNamePoi = addressPOI.getString("localName");
+               //System.out.println("String data from the 'localName' object within the 'address' object within the 'poi' object: " + localNamePoi);
+
+
+               // Retrieve the 'lat' and 'lon' information of type double from the JSONObject called 'position'
+               JSONObject positionPoi = resultArrayObj.getJSONObject(0).getJSONObject("position");
+               double latPoi = positionPoi.getDouble("lat");
+               double lonPoi = positionPoi.getDouble("lon");
+
+               return new PoiImpl(namePoi, categoryIdPoi, addressPoi, localNamePoi, latPoi, lonPoi);
+          }
+
+          private void readIncomingData(BufferedReader bufferedReaderObject, StringBuilder stringBuilderObject) throws IOException {
+               String line;
+               while ((line = bufferedReaderObject.readLine()) != null) {
+                    stringBuilderObject.append(line);
+               }
+          }
+
+          private URL getUrl() throws MalformedURLException {
+               return new URL(urlBuildService.getUrlSearchString());
+          }
+}
