3a7562c439e935672f137dee0e15db446cde8bac
RonaldVanDenBurg <60896718+RonaldVanDenBurg@users.noreply.github.com>
2022-01-12 15:21:42 +0100

    Edited the UrlBuildServiceImpl class

--- src/main/java/com/tomtomscenery/scenery_backend/services/I_UrlBuildService.java
+++ src/main/java/com/tomtomscenery/scenery_backend/services/I_UrlBuildService.java
@@ -3,4 +3,6 @@ package com.tomtomscenery.scenery_backend.services;
 public interface I_UrlBuildService {
      String getUrlSearchString();
      void setUrlSearchString(String category, double lat, double lon);
+     int getLimit();
+     void setLimit(int limit);
 }
--- src/main/java/com/tomtomscenery/scenery_backend/services/impl/UrlBuildServiceImpl.java
+++ src/main/java/com/tomtomscenery/scenery_backend/services/impl/UrlBuildServiceImpl.java
@@ -1,16 +1,21 @@
 package com.tomtomscenery.scenery_backend.services.impl;
 
+
 import com.tomtomscenery.scenery_backend.services.I_UrlBuildService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UrlBuildServiceImpl implements I_UrlBuildService
 {
+
      private String urlSearchString;
+     private int limit = 10;
 
+     public UrlBuildServiceImpl() {
+     }
 
-
-     // This method gathers all the information from the GET-Request to form a String (urlSearchString).
+     // This method gathers all the information from the GET-Request (String category, double lat, double lon),
+     // to form a String (urlSearchString).
      // This String will be used to search for Points Of Interest via the TomTom API.
      @Override
      public void setUrlSearchString(String category, double lat, double lon)
@@ -18,14 +23,24 @@ public class UrlBuildServiceImpl implements I_UrlBuildService
           String baseUrl      = "https://api.tomtom.com/search/2/categorySearch/";
           String key          = "akRRoJXWg8BsEebGgXquaebxtD95DZ2d";
           String lang         = "nl-NL";
-          int radius          = 1000;
-          int limit           = 1;
-          String urlSearchString =  baseUrl + category + ".json?key=" + key + "&language=" + lang + "&lat=" + lat + "&lon=" + lon + "&radius=" + radius + "&limit=" + limit;
-          this.urlSearchString = urlSearchString;
+          int radius          = 10000;
+          this.urlSearchString = baseUrl + category + ".json?key=" + key + "&language=" + lang + "&lat=" + lat + "&lon=" + lon + "&radius=" + radius + "&limit=" + limit;
      }
+
+     @Override
+     public int getLimit() {
+          return limit;
+     }
+
+     @Override
+     public void setLimit(int limit) {
+          this.limit = limit;
+     }
+
 
      @Override
      public String getUrlSearchString() {
           return urlSearchString;
      }
 }
+
